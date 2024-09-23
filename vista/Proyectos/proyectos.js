92b0851adf1e8c378b479d28068c313d959cd784
Carlos Jhoan Aguilar Galvis <carlosjhoanaguilar@gmail.com>
2024-09-23 07:51:15 -0500

    Ya se pudo cargar el script de menu.js

--- vista/Proyectos/proyectos.js
+++ vista/Proyectos/proyectos.js
@@ -1,16 +1,10 @@
-
-
-
-
 document.body.setAttribute("onload", "cargar()");
   
-
 function cargar() {
-    
-    document.getElementById("menu-inicio").setAttribute("onclick", "irAHome()");
-    
+      
 }
 
 function irAHome() {
+    
     location.href="/vista/Home/home.html";
 }
--- vista/menu.js
+++ vista/menu.js
@@ -2,7 +2,7 @@ fetch('/vista/menu.html')
 .then(response => response.text())
 .then(data => {
         document.body.innerHTML += data;
-        document.body.setAttribute("onload", "cargar()");
+        document.getElementById("menu-inicio").setAttribute("onclick", "irAHome()");
 
     }
 );
