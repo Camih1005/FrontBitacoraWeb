8c9bed9ad26afd6514831bfc676c564ab67dd698
Carlos Jhoan Aguilar Galvis <carlosjhoanaguilar@gmail.com>
2024-09-23 02:01:04 -0500

    Batallando con los scripts, que no andan cargando

--- vista/Proyectos/proyectos.js
+++ vista/Proyectos/proyectos.js
@@ -1,4 +1,13 @@
-document.getElementById("menu-inicio").setAttribute("onclick", "irAHome()");
+
+
+
+document.body.setAttribute("onload", "cargarProyectos()")
+
+function cargarProyectos() {
+    console.log("Hola")
+    document.getElementById("menu-inicio").setAttribute("onclick", "irAHome()");
+    
+}
 
 function irAHome() {
     location.href="/vista/Home/home.html"
