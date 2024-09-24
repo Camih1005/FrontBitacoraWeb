df5b094843f64a5df9b5fcd2dcdd17fcf5a01242
Carlos Jhoan Aguilar Galvis <carlosjhoanaguilar@gmail.com>
2024-09-24 02:26:03 -0500

    Formulario culminado con funcionalidad de subir foto

--- vista/Team/team.js
+++ vista/Team/team.js
@@ -7,7 +7,31 @@ let usuario = {
 usuario.rol = localStorage.getItem("rol");
 
 document.body.setAttribute("onload", "cargar()");
+document.getElementById("bot-registrar").setAttribute("onclick", "desplegarFormularioRegistro()")
+document.getElementById("bot-cancelar").setAttribute("onclick", "cerrarFormularioRegistro()")
+document.getElementById("foto-perfil-registro").setAttribute("onchange", "subirFoto()")
+
 
 function cargar() {  
-
+    console.log(document.getElementById("foto-perfil-registro").textContent)
 }
+
+function desplegarFormularioRegistro() {
+    let divFormulario = document.getElementById("registrar-integrante"); 
+    divFormulario.style.display="flex"
+    
+    
+}
+
+function cerrarFormularioRegistro() {
+    let divFormulario = document.getElementById("registrar-integrante"); 
+    divFormulario.style.display="none";
+    
+}
+
+function subirFoto() {
+    let nombreFotoPerfil =document.getElementById("foto-perfil-registro").value.split("\\")[2] 
+    document.getElementById("nombre-foto-perfil").value = nombreFotoPerfil;
+    
+}
+
