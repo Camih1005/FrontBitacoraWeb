94cbcbc411099336995636861ea101a05c9324d6
Carlos Jhoan Aguilar Galvis <carlosjhoanaguilar@gmail.com>
2024-09-24 09:53:50 -0500

    Avanzando en el registro

--- vista/Team/team.js
+++ vista/Team/team.js
@@ -10,7 +10,16 @@ document.body.setAttribute("onload", "cargar()");
 document.getElementById("bot-registrar").setAttribute("onclick", "desplegarFormularioRegistro()")
 document.getElementById("bot-cancelar").setAttribute("onclick", "cerrarFormularioRegistro()")
 document.getElementById("foto-perfil-registro").setAttribute("onchange", "subirFoto()")
-document.getElementById("bot-confirmar").setAttribute("onclick", "realizarRegistro()")
+document.getElementById("bot-confirmar").setAttribute("onclick", "realizarRegistro()");
+
+let nombreInput =  document.getElementById("nombre");
+nombreInput.setAttribute("onclick", "cambiarColor(nombreInput)")
+let usuarioInput = document.getElementById("usuario");
+usuarioInput.setAttribute("onclick", "cambiarColor(usuarioInput)")
+let emailInput = document.getElementById("email");
+emailInput.setAttribute("onclick", "cambiarColor(emailInput)")
+let identificacionInput = document.getElementById("identificacion");
+identificacionInput.setAttribute("onclick", "cambiarColor(identificacionInput)")
 
 
 function cargar() {  
@@ -39,12 +48,14 @@ function subirFoto() {
 function realizarRegistro() {
 
     let infoRegistro;
-    let nombre =  document.getElementById("nombre").value.trim();
-    let usuario = document.getElementById("usuario").value.trim();
-    let email = document.getElementById("email").value.trim();
-    let identificacion = document.getElementById("identificacion").value.trim();
+    nombreInput =  document.getElementById("nombre");
+    let nombre =  nombreInput.value.trim();
+    let usuario = usuarioInput.value.trim();
+    let email = emailInput.value.trim();
+    let identificacion = identificacionInput.value.trim();
     let idRol = document.getElementById("select-rol").value;
-    let nombreFoto = document.getElementById("nombre-foto-perfil").value;
+    let nombreFotoInput = document.getElementById("nombre-foto-perfil");
+    let nombreFoto = nombreFotoInput.value;
 
     infoRegistro = {
         "nombre" : `${nombre}`,
@@ -55,21 +66,30 @@ function realizarRegistro() {
         "foto" : `${nombreFoto}`
     }
 
-    const regex_name = /^[a-zA-Z\s]+$/; //Para comprobar nombres
-    const regex_whitespace = /\S/; //Regex para comprobar que no esté vacío
-    //const regex_identificacion = /^[0-9]+$/; //regex para comprobar el número de teléfono
-    const regex_email = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g; //reex para comprobar el email
+    // const regex_name = /^[a-zA-Z\s]+$/; //Para comprobar nombres
+    // const regex_whitespace = /\S/; //Regex para comprobar que no esté vacío
+    // //const regex_identificacion = /^[0-9]+$/; //regex para comprobar el número de teléfono
+    // const regex_email = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g; //reex para comprobar el email
 
-    arr_id_input = [["nombre", Boolean(regex_whitespace.test(name_value)*regex_name.test(name_value))],
-                    ["email", regex_email.test(email_value) ] ,
-                    ["celular", regex_cel.test(celular_value)] ];
+    // arr_id_input = [["nombre", Boolean(regex_whitespace.test(name_value)*regex_name.test(name_value))],
+    //                 ["email", regex_email.test(email_value) ] ,
+    //                 ["celular", regex_cel.test(celular_value)] ];
+
+    if (nombre == "") {
+        console.log(nombreInput.value)
+        nombreInput.value = "Ingrese un nombre";
+        nombreInput.style.color = "red"
+    }
 
 
-    console.log(infoRegistro);
+}
 
 
+function cambiarColor(elementoInput) {
+    if (elementoInput.style.color=="red") {
+        elementoInput.style.color = "black";
+        elementoInput.value="";
+    }
     
-
-
 }
 
