8a6a15f758e0ffaa5cfbceb07fdef25f2c1e12ed
camih1005 <camiloht0918@gmail.com>
2024-09-26 08:31:05 -0500

    changes

--- vista/Usuario/js/filtroAvanzado.js
+++ vista/Usuario/js/filtroAvanzado.js
@@ -82,7 +82,7 @@ function initializeFilterSystem() {
 
     document.body.insertAdjacentHTML('beforeend', `
         <div class="filtroContainer my-5">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-topw">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">Actividades</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
